c8c66b7c8f5dc230ebc7290f3071ad8b31329ee7
nuthan <inbox.nuthan@gmail.com>
2024-02-10 01:46:06 +0530

    In Regions Controller - Removed unwanted code in the file.

--- InDepthUnderstanding/WalkMe.API/Controllers/RegionsController.cs
+++ InDepthUnderstanding/WalkMe.API/Controllers/RegionsController.cs
@@ -21,37 +21,10 @@ namespace WalkMe.API.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            // Approach 1: Without DBContext
-            //var regions = new List<Region>
-            //{
-            //    new() {
-            //        Id = Guid.NewGuid(),
-            //        Name= "Region 1",
-            //        Code="NASA",
-            //    },
-            //    new() {
-            //        Id = Guid.NewGuid(),
-            //        Name= "Region 2",
-            //        Code="SANA",
-            //    }
-            //};
-
-            // Approach 2: Get data from a table
-
-            //var regions = dbContext.Regions;
-            // SQL Query: SELECT [r].[Id], [r].[Code], [r].[Name], [r].[RegionImageUrl]
-            // FROM[Regions] AS[r]
-            //var regions1 = dbContext.Regions.ToList();
-            // SQL Query: SELECT [r].[Id], [r].[Code], [r].[Name], [r].[RegionImageUrl]
-            // FROM[Regions] AS[r]
-
             // Get Data from database - Domain
             var regions = dbContext.Regions;
 
             // Map domain data to Dto
-            //var regionDto = regions.ToList();
-
-            // Traditional way
             var regionDto = new List<RegionDto>();
             foreach (var regionDomain in regions)
             {
@@ -76,17 +49,6 @@ namespace WalkMe.API.Controllers
             // Option 1: 
             var regionDomain = dbContext.Regions.Find(id);
 
-
-            // Option 2: Combined using Where and FirstOrDefault
-            //var regionWithWhere = dbContext.Regions.Where(x => x.Id == id).FirstOrDefault();
-
-            // Option 3: 
-            //var regionWithFirstOrDefault = dbContext.Regions.FirstOrDefault(x => x.Id == id);
-
-            // Option 4: 
-            //var regionWithSingleOrDefault = dbContext.Regions.SingleOrDefault(x => x.Id == id);
-
-
             if (regionDomain is null)
             {
                 return NotFound();
@@ -99,7 +61,6 @@ namespace WalkMe.API.Controllers
                 Code = regionDomain.Code,
                 RegionImageUrl = regionDomain.RegionImageUrl
             };
-
 
             return Ok(regionDto);
         }
@@ -133,14 +94,6 @@ namespace WalkMe.API.Controllers
             };
 
             return CreatedAtAction(nameof(GetById), new { Id = regionDto.Id }, regionDto);
-
-            // SQL Query
-            /*  SET IMPLICIT_TRANSACTIONS OFF;
-                SET NOCOUNT ON;
-                INSERT INTO[Regions] ([Id], [Code], [Name], [RegionImageUrl])
-                VALUES(@p0, @p1, @p2, @p3);
-            */
-
         }
 
         // Put: Update a existing region
@@ -180,15 +133,6 @@ namespace WalkMe.API.Controllers
             };
 
             return CreatedAtAction(nameof(GetById), new { Id = regionDto.Id }, regionDto);
-
-            // SQL Query
-            /*   SET IMPLICIT_TRANSACTIONS OFF;
-                 SET NOCOUNT ON;
-                 UPDATE [Regions] SET [Code] = @p0, [Name] = @p1, [RegionImageUrl] = @p2
-                 OUTPUT 1
-                 WHERE [Id] = @p3;
-            */
-
         }
 
         // Put: Update a existing region
@@ -209,15 +153,6 @@ namespace WalkMe.API.Controllers
             dbContext.SaveChanges();
 
             return NoContent();
-
-            // SQL Query
-            /*  SET IMPLICIT_TRANSACTIONS OFF;
-                SET NOCOUNT ON;
-                DELETE FROM [Regions]
-                OUTPUT 1
-                WHERE [Id] = @p0;
-            */
-
         }
     }
 }
