3757d4c16b8076122da0a05f2dda517fad5619d5
nuthan <inbox.nuthan@gmail.com>
2024-02-10 13:11:57 +0530

    Order of methods changed into action flow.

--- InDepthUnderstanding/WalkMe.API/Repositories/RegionRepository.cs
+++ InDepthUnderstanding/WalkMe.API/Repositories/RegionRepository.cs
@@ -11,25 +11,6 @@ namespace WalkMe.API.Repositories
         {
             this.dbContext = dbContext;
         }
-        public async Task<Region> CreateRegionAsync(Region region)
-        {
-            await dbContext.Regions.AddAsync(region);
-            await dbContext.SaveChangesAsync();
-            return region;
-        }
-
-        public async Task<Region?> DeleteRegionAsync(Guid regionId)
-        {
-            var regionData = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == regionId);
-            if (regionData is null)
-            {
-                return null;
-            }
-
-            dbContext.Regions.Remove(regionData);
-            await dbContext.SaveChangesAsync();
-            return regionData;
-        }
 
         public async Task<List<Region>> GetAllAsync()
         {
@@ -39,6 +20,13 @@ namespace WalkMe.API.Repositories
         public async Task<Region?> GetByIdAsync(Guid regionId)
         {
             return await dbContext.Regions.FindAsync(regionId);
+        }
+
+        public async Task<Region> CreateRegionAsync(Region region)
+        {
+            await dbContext.Regions.AddAsync(region);
+            await dbContext.SaveChangesAsync();
+            return region;
         }
 
         public async Task<Region?> UpdateRegionAsync(Guid regionId, Region region)
@@ -56,5 +44,19 @@ namespace WalkMe.API.Repositories
             await dbContext.SaveChangesAsync();
             return regionData;
         }
+
+        public async Task<Region?> DeleteRegionAsync(Guid regionId)
+        {
+            var regionData = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == regionId);
+            if (regionData is null)
+            {
+                return null;
+            }
+
+            dbContext.Regions.Remove(regionData);
+            await dbContext.SaveChangesAsync();
+            return regionData;
+        }
+
     }
 }
